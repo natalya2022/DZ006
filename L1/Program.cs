ab39000cda0f4a02a941dba87cabd5219014ab88
Natalya <buzlik.n@gmail.com>
2022-08-25 17:34:54 +0400

    Задачи ДЗ 6

---
+++ L1/Program.cs
@@ -0,0 +1,67 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                    // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
+
+
+void OddsCheck(int number)              // Функция проверки корректности условий
+{
+    if (number > 0)
+    {
+        int[] myArr = ArrayInput(number);
+        ArrayPrint(myArr);
+        System.Console.WriteLine($"Количество положительных чисел равно {PositiveNumber(myArr)}");
+    }
+    else
+    {
+        System.Console.WriteLine("Количество не может быть отрицательным или равным нулю");
+    }
+}
+
+
+int[] ArrayInput(int number)                    // Функция ввода массива
+{
+    int[] myArr = new int[number];
+    int i = 0;
+    while (i < number)
+    {
+        Console.Write("Введите число ");
+        myArr[i] = int.Parse(Console.ReadLine());
+        i++;
+    }
+    return myArr;
+}
+
+
+int PositiveNumber(int[] myArr)            // Функция подсчета положительных чисел
+{
+    int count = 0;
+    for (int i = 0; i < myArr.Length; i++)
+    {
+        if (myArr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void ArrayPrint(int[] myArr)                            // Функция печати массива    
+{
+    for (int j = 0; j < myArr.Length; j++)
+    {
+        System.Console.Write($"{myArr[j]}  ");
+    }
+}
+
+
+int number = Prompt("Определите количество чисел для ввода ");  // Определяем количество элементов (М) массива
+OddsCheck(number);                                 // Начинаем проверку условий
+
---
+++ L2/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                    // Вывести сообщение
+    int result = Convert.ToInt32(Console.ReadLine()); // Считывает значение
+    return result;                                    // Возвращает результат
+}
+
+void OddsCheck(double k1, double b1, double k2, double b2)  // Функция проверки условий
+{
+    if (b1 == b2 && k1 == k2)
+    {
+        System.Console.WriteLine("Прямые совпадают");
+    }
+    else if (k1 == k2)
+    {
+        System.Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        var tuple = SystemSolution(k1, b1, k2, b2);
+        Console.WriteLine($"Координаты точки пересечения ({(tuple.Item1):f2}; {(tuple.Item2):f2})");
+    }
+}
+
+(double, double) SystemSolution(double k1, double b1, double k2, double b2) // Функция находит точку пересечения прямых
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    var result = (x, y);
+    return result;
+}
+
+int k1 = Prompt("Определите коэффициент k1 ");          // Ввводим коэффициенты
+int b1 = Prompt("Определите коэффициент b1 ");
+int k2 = Prompt("Определите коэффициент k2 ");
+int b2 = Prompt("Определите коэффициент b2 ");
+OddsCheck(k1, b1, k2, b2);                              // Начинаем проверку условий
+
